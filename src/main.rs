34fdebc124ff35dee28d4da6513ee9632cbb89f1
Tadeo Aguilar <Tadeo.Aguilar@FINNING.COM>
2023-01-30 11:14:31 -0500

    'Btree-fix'

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::fmt::Debug;
 
 
-type ChildNode<T> = Option<Box<BinData<T>>>;
+
 pub struct BinData<T> {
    pub data: T,
-   pub left: ChildNode<T>,
-   pub right: ChildNode<T>
+   pub left: BinTree<T>,
+   pub right:BinTree<T>
    
 }
 
@@ -14,6 +14,48 @@ pub struct BinTree<T> (Option<Box<BinData<T>>>);
 impl<T> BinTree<T> {
     pub fn new() -> Self {
         BinTree(None)
+    }
+    pub fn rot_left(&mut self){
+        self.0 = self.0.take().map(|v| v.rot_left())
+    }
+    pub fn rot_right(&mut self){
+        self.0 = self.0.take().map(|v| v.rot_right())
+    }
+
+}
+
+
+impl<T> BinData<T> {
+    pub fn rot_left(mut self)-> Box<Self> {
+        let mut res = match self.right.0.take(){
+            Some(res)=> res,
+            None => return Box::new(self)
+        };
+        self.right = BinTree(res.left.0.take());
+        res.left = BinTree(Some(Box::new(self)));
+        res
+    }
+    pub fn rot_right(mut self)-> Box<Self> {
+        let mut res = match self.left.0.take(){
+            Some(res)=> res,
+            None => return Box::new(self)
+        };
+        self.right = BinTree(res.left.0.take());
+        res.left = BinTree(Some(Box::new(self)));
+        res
+    }
+}
+impl<T:Debug> BinTree<T>{
+    pub fn lfirst_print(&self,depth:i32){
+        if let Some(ref bd)=self.0{
+                bd.left.lfirst_print(depth+1);
+                let mut spc = String::new();
+                for _ in 0..depth{
+                    spc.push(' ');
+                }
+                println!("{}{:?}",spc,bd.data);
+                bd.right.lfirst_print(depth+1);
+        }
     }
 }
 impl<T: PartialOrd> BinTree<T>{
@@ -41,8 +83,25 @@ impl<T: PartialOrd> BinTree<T>{
 }
 
 
+
+
 fn main(){
-    print!("Hola");
+    let mut t = BinTree::new();
+    t.add_sorted(4);
+    t.add_sorted(6);
+    t.add_sorted(1);
+    t.add_sorted(9);
+    t.add_sorted(8);
+    t.add_sorted(13);
+    t.add_sorted(45);
+    t.add_sorted(0);
+    t.add_sorted(20);
+   
+    //println!("tree = {:?}",t);
+    t.lfirst_print(0);
+    println!(" -- rotleft -- ");
+    t.rot_left();
+    t.lfirst_print(0);
 }
 
 
